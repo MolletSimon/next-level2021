3f14e189c5564121c2316794aa3b5523cfdcf7fa
Simon Mollet <mollet.simon.pro@gmail.com>
2021-07-23 01:26:15 +0200

    add navbar and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InscriptionsComponent } from './inscriptions/inscriptions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InscriptionsComponent
   ],
   imports: [
     BrowserModule,
